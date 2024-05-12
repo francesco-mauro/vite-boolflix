77c5fa2e4d20bcc591863110ded77de91681fe0c
francesco-mauro <francesco.mauro180@gmail.com>
2024-05-13 01:04:47 +0200

    add: working card skeleton

---
+++ src/components/AppCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    props: {
+        cardObj: Object,
+    },
+};
+
+</script>
+
+<template>
+    <div>
+        <h4> {{ cardObj.title }} </h4>
+        <p>{{ cardObj.original_title }}</p>
+        <p>{{ cardObj.original_language }}</p>
+        <p>{{ cardObj.vote_average }}</p>
+    </div>
+</template>
+
+<style>
+
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,13 @@
 <script>
 // STORE è TRA PARENTESI GRAFFE PERCHè STIAMO IMPORTANDO UNA DI UNA SERIE DI VARIABILI, QUANDO SI POSSONO ESPORTARE VARIABILI MULTIPLE SI USANO LE PARENTESI GRAFFE, PER QUESTO SPECIFICHIAMO STORE, PERCHè IN STORE POTREBBERO FINIRCI ANCHE ALTGRE VARIABILI. POSSO USARE LA VIRGOLA PER IMPORTARE VARIABILI MULTIPLE: EX {store, myVar}. Senza parentesi graffe prendo ---TUTTO---, come accade per i vari AppXxxxx
 import {store} from "../store.js";
+import AppCard from "./AppCard.vue";
+
 
 export default{
+    components: {
+        AppCard
+    },
     data () {
         return{
             store
@@ -16,7 +21,9 @@ export default{
         <div>
             <h2>Film</h2>
             <div v-for = "movie in store.moviesArray">
-                {{ movie.title }}
+               <AppCard :cardObj="movie" />
+               <!-- verifica di funzionamento movie  -->
+                <!-- {{ movie.title }} -->
             </div>
         </div>
     </main>
